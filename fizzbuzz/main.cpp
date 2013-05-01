691686007dd9e79944489355aac1445f844a22bf
Tony <CodeGuro@gmail.com>
2013-05-01 01:49:44 -0400

    fizzbuzz test

---
+++ fizzbuzz/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main(int const argc, char const * const * argv)
+{
+	for(unsigned i = 1; i < 101; ++i)
+	{
+		if( !(i % 3) )
+			std::cout << "fizz\n";
+		else if( !(i % 5) )
+			std::cout << "buzz\n";
+		else if( !(i % 15) )
+			std::cout << "fizzbuzz\n";
+		else
+			std::cout << i << std::endl;
+	}
+	std::cin.get();
+	return EXIT_SUCCESS;
+}
